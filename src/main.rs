9f2fc513818643fb35d2a65f5c92fdc3c1b18a24
qazal <qazal.software@gmail.com>
2024-06-15 16:07:04 +0300

    configure args

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use clap::{command, Parser, Subcommand};
 
-#[derive(Subcommand, Clone, Debug)]
+#[derive(Subcommand, Debug)]
 pub enum Command {
-    Start,
+    Start(StartArgs),
     Ls,
-    Watch,
+    Check(CheckArgs),
 }
 
 #[derive(Parser, Debug)]
@@ -13,7 +13,26 @@ pub struct Cli {
     command: Command,
 }
 
+#[derive(Parser, Debug)]
+pub struct StartArgs {
+    #[arg(short, help = "Add a string parameter in key=value format", long)]
+    field: String,
+}
+
+#[derive(Parser, Debug)]
+pub struct CheckArgs {
+    #[arg(short, help = "Poll output every 10s", long)]
+    watch: bool,
+    #[arg(short, help = "Notify once done", long)]
+    notify: bool,
+}
+
 fn main() {
     let args = Cli::parse();
-    println!("{:?}", args);
+
+    match args.command {
+        Command::Start(_) => todo!(),
+        Command::Ls => todo!(),
+        Command::Check(_) => todo!(),
+    }
 }
