b0d4de472ca019b079b90a9726ae0149c1685f6b
qazal <qazal.software@gmail.com>
2024-06-15 20:31:59 +0300

    start workflow

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{command, Parser, Subcommand};
+use std::error::Error;
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
@@ -15,8 +16,15 @@ pub struct Cli {
 
 #[derive(Parser, Debug)]
 pub struct StartArgs {
-    #[arg(short, help = "Add a string parameter in key=value format", long)]
-    field: String,
+    #[arg(short, help = "The workflow .yml file", long)]
+    workflow_id: String,
+    #[arg(
+        short,
+        help = "Add a string parameter in key=value format",
+        long,
+        required = false
+    )]
+    field: Option<String>,
 }
 
 #[derive(Parser, Debug)]
@@ -27,12 +35,34 @@ pub struct CheckArgs {
     notify: bool,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
+    let token = std::env::var("GH_TOKEN").unwrap();
+    let repo = std::env::var("REPO").unwrap();
 
     match args.command {
-        Command::Start(_) => todo!(),
+        Command::Start(args) => {
+            let client = reqwest::Client::new();
+            let body = serde_json::json!({"ref": "process_replay_everytime"});
+            let res = client
+                .post(format!(
+                    "https://api.github.com/repos/{repo}/actions/workflows/{}/dispatches",
+                    args.workflow_id
+                ))
+                .header("User-Agent", "test")
+                .header("Accept", "application/vnd.github.v3+json")
+                .header("Authorization", format!("Bearer {}", token))
+                .json(&body)
+                .send()
+                .await?;
+            if res.status() != 204 {
+                return Err(format!("couldn't create workflow {}", res.status()).into());
+            }
+        }
         Command::Ls => todo!(),
         Command::Check(_) => todo!(),
     }
+
+    Ok(())
 }
